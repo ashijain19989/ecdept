6f61e5dae2359a2db2ac25f038e92db8203f0632
ashi jain <ashijain19989@gmail.com>
2019-07-31 11:26:26 +0530

    ec department design

---
+++ ecdept.js
@@ -0,0 +1,20 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.f-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.bb-rec').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
